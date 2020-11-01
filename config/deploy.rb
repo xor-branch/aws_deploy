98b73d106ac4a8e126141d02d3c57905611dcaa4
baroka <birotori@gmail.com>
2020-11-01 21:10:43 +0100

    ok

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,8 +9,8 @@ set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
-set :rvm_type, :user
-set :rvm_ruby_version, 'ruby-2.6.5'
+set :rbenv_ruby , ' 2.6.5 '
+set :rbenv_type , :système
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
