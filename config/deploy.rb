540114540023d66118a255afc2fc7cbec37d3422
baroka <birotori@gmail.com>
2020-11-26 17:53:54 +0100

    deploy.rb fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,10 @@ set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
-set :rvm_type, :user
-set :rvm_ruby_version, 'ruby-2.6.5' # Edit this if you are using MRI Ruby
+#set :rvm_type, :user
+#set :rvm_ruby_version, 'ruby-2.6.5' # Edit this if you are using MRI Ruby
+set :rbenv_ruby, '2.6.5'
+set :rbenv_type, :system# Edit this if you are using MRI Ruby
 
 set :log_level, :info
 namespace :deploy do
