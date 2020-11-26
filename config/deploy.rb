2f7878ebb81f168eb02ba5c299703b0070d22ead
baroka <birotori@gmail.com>
2020-11-26 22:04:40 +0100

    d

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,33 +10,26 @@ set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
-#set :rvm_type, :user
-#set :rvm_ruby_version, 'ruby-2.6.5' # Edit this if you are using MRI Ruby
-set :rbenv_ruby, '2.6.5'
-set :rbenv_type, :system # Edit this if you are using MRI Ruby
+set :rvm_type, :user
+set :rvm_ruby_version, 'ruby-2.6.5' # Edit this if you are using MRI Ruby
+#set :rbenv_ruby, '2.6.5'
+#set :rbenv_type, :system # Edit this if you are using MRI Ruby
 
-set :log_level, :info
-namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-  desc 'Create database'
-  task :db_create do
-    on roles(:db) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:create'
-        end
-      end
-    end
-  end
-  after :publishing, :restart
-   after :restart, :clear_cache do
-     on roles(:web), in: :groups, limit: 3, wait: 10 do
-     end
-   end
- end
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 8]
+set :puma_workers, 0
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+set :puma_preload_app, false
+
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
